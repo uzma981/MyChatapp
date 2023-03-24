407931843bfc6ec29397f87ee7c7d3cde8fd0053
Uzma89 <uzma.a786@hotmail.com>
2023-03-24 16:52:26 +0000

    formatted date in chat item for last message

--- components/Chats/ChatItem.js
+++ components/Chats/ChatItem.js
@@ -1,26 +1,25 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
-
+import moment from "moment";
 import globalStyle from "../global-style";
 export default function ChatItem({ chat, navigation }) {
   const handleChatPress = () => {
     navigation.navigate("Single Chat", { chatId: chat.chat_id });
+  };
+  const formatDate = (timestamp) => {
+    return moment(timestamp).format("hh:mm A");
   };
   return (
     <TouchableOpacity
       onPress={handleChatPress}
       style={globalStyle.singlecontainer}
     >
-      <Image
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-        }}
-        style={styles.image}
-      />
       <View style={globalStyle.singlecontainerContent}>
         <View style={globalStyle.singlecontainerRow}>
           <Text style={styles.name}>{chat.name}</Text>
-          <Text style={styles.subTitle}>{chat.last_message.timestamp}</Text>
+          <Text style={styles.subTitle}>
+            {formatDate(chat.last_message.timestamp)}
+          </Text>
         </View>
         <Text numberOfLines={2} style={styles.subTitle}>
           {chat.last_message.author.first_name}: {chat.last_message.message}
