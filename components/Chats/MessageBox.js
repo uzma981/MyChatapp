52b15662ff2075dd6c01d30430870da39ce6ad10
Uzma89 <uzma.a786@hotmail.com>
2023-04-10 15:40:00 +0100

    save msg as draft after 5secs functionality

--- components/Chats/MessageBox.js
+++ components/Chats/MessageBox.js
@@ -1,11 +1,41 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
-  View, TextInput, TouchableOpacity, StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 function MessageBox({ message, setMessage, sendMessage }) {
+  const [typingTimer, setTypingTimer] = useState(null);
+  const saveDraft = async (draft) => {
+    try {
+      const storedDrafts = await AsyncStorage.getItem('messageDrafts');
+      const parsedDrafts = storedDrafts ? JSON.parse(storedDrafts) : [];
+      const updatedDrafts = [...parsedDrafts, draft];
+      await AsyncStorage.setItem(
+        'messageDrafts',
+        JSON.stringify(updatedDrafts),
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChangeText = (text) => {
+    setMessage(text);
+    clearTimeout(typingTimer);
+    if (text.length > 0) {
+      setTypingTimer(
+        setTimeout(() => {
+          saveDraft(text);
+        }, 5000),
+      );
+    }
+  };
+
   const styles = StyleSheet.create({
     container: {
       alignItems: 'baseline',
@@ -32,13 +62,27 @@ function MessageBox({ message, setMessage, sendMessage }) {
       justifyContent: 'center',
       alignItems: 'center',
     },
+    draftItem: {
+      backgroundColor: '#F5F5F5',
+      padding: 5,
+      marginVertical: 5,
+      borderRadius: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    draftText: {
+      color: '#A9A9A9',
+      flex: 1,
+      marginHorizontal: 5,
+    },
   });
+
   return (
     <View style={styles.container}>
       <View style={styles.mainContainer}>
         <TextInput
           value={message}
-          onChangeText={(text) => setMessage(text)}
+          onChangeText={onChangeText}
           style={styles.textInput}
           autoFocus
         />
@@ -48,6 +92,14 @@ function MessageBox({ message, setMessage, sendMessage }) {
           </View>
         </TouchableOpacity>
       </View>
+      {/* {drafts.length > 0 && (
+        <FlatList
+          data={drafts}
+          renderItem={renderDraftItem}
+          keyExtractor={(item, index) => index.toString()}
+          style={{ maxHeight: 100 }}
+        />
+      )} */}
     </View>
   );
 }
