dd252bed055b9773c64f5e1fff95b1c7597c2e1e
Uzma89 <uzma.a786@hotmail.com>
2023-04-10 15:40:19 +0100

    eslint refactor

--- components/Form/LoginForm.js
+++ components/Form/LoginForm.js
@@ -1,18 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
 import {
   Text,
   TextInput,
   View,
   TouchableOpacity,
-  StyleSheet,
-} from "react-native";
-import globalStyle from "../global-style";
-import { Formik } from "formik";
-import { baseSchema } from "./Validation/validationschema";
+} from 'react-native';
+import { Formik } from 'formik';
+import globalStyle from '../global-style';
+import { baseSchema } from './Validation/validationschema';
 
 export default function LoginForm({ handleLogin, navigation, displayMessage }) {
   return (
     <Formik
-      initialValues={{ email: "", password: "" }}
+      initialValues={{ email: '', password: '' }}
       validationSchema={baseSchema}
       onSubmit={handleLogin}
     >
@@ -25,33 +25,34 @@ export default function LoginForm({ handleLogin, navigation, displayMessage }) {
         touched,
       }) => (
         <View style={globalStyle.welcomecontainer}>
-          <Text style={{ fontSize: 24, fontWeight: "bold" }}>
-            WhatsThat<Text style={{ color: "#fb5b5a" }}> App</Text>
+          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+            WhatsThat
+            <Text style={{ color: '#fb5b5a' }}> App</Text>
           </Text>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Login</Text>
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Login</Text>
           <TextInput
             style={globalStyle.inputBox}
             placeholder="Email"
             keyboardType="email-address"
-            onChangeText={handleChange("email")}
-            onBlur={handleBlur("email")}
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
             value={values.email}
           />
           {errors.email && touched.email && (
-            <Text style={{ color: "red" }}>{errors.email}</Text>
+            <Text style={{ color: 'red' }}>{errors.email}</Text>
           )}
 
           <TextInput
             style={globalStyle.inputBox}
             placeholder="Password"
             keyboardType="visible-password"
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
+            onChangeText={handleChange('password')}
+            onBlur={handleBlur('password')}
             value={values.password}
-            secureTextEntry={true}
+            secureTextEntry
           />
           {errors.password && touched.password && (
-            <Text style={{ color: "red" }}>{errors.password}</Text>
+            <Text style={{ color: 'red' }}>{errors.password}</Text>
           )}
 
           <TouchableOpacity
@@ -69,7 +70,7 @@ export default function LoginForm({ handleLogin, navigation, displayMessage }) {
           </TouchableOpacity>
           <TouchableOpacity
             title="SignUpText"
-            onPress={() => navigation.navigate("SignUp")}
+            onPress={() => navigation.navigate('SignUp')}
             style={{ marginBottom: 10 }}
           >
             <View style={{ padding: 10 }}>
@@ -77,11 +78,10 @@ export default function LoginForm({ handleLogin, navigation, displayMessage }) {
             </View>
           </TouchableOpacity>
           <View>
-            <Text style={{ color: "red" }}>{displayMessage}</Text>
+            <Text style={{ color: 'red' }}>{displayMessage}</Text>
           </View>
         </View>
       )}
     </Formik>
   );
 }
-const styles = StyleSheet.create({});
